85d939f3e1a39b438606a4ab9de643b1f0645fe0
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-07-12 16:44:04 +0200

    path correct

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-snakemake 00-scripts/snakeMake --singularity-args "-B .:/workf,/entrepot/donnees/seaconnect/gbs_diplodus/tiny/:/02-raw ," --use-singularity
+snakemake -s 00-scripts/snakeMake -j 8 --singularity-args "-B .:/workf,/entrepot/donnees/seaconnect/gbs_diplodus/tiny/:/02-raw" --use-singularity
