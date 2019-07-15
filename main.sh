1f1ab64d3bd04f6d1e38eb8229c5e59f64cc95a1
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-07-15 10:35:33 +0200

    nadd casava to container

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-snakemake -s 00-scripts/snakeMake -j 8 --singularity-args "-B .:/workf,/entrepot/donnees/seaconnect/gbs_diplodus/tiny/:/02-raw" --use-singularity
+snakemake -s 00-scripts/snakefile -j 8 --use-singularity
