521d77751d6f3ebf9c27ccf2558f3cdf1ef72b8c
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-07-15 16:18:16 +0200

    folder gunzip

--- main.sh
+++ main.sh
@@ -2,3 +2,4 @@ snakemake -s 00-scripts/snakefile -j 8 --use-singularity --configfile 01-infos/d
 ## clean all files
 rm 02-fastq/* 03-illumina_filter/* 04-phix_filter/* 05-trimmed/*
 rm -Rf 10-logs/*
+rm -Rf .snakemake
