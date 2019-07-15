f0ebcfaa810ec8d3375e9dcd415f678453bd695e
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-07-15 16:10:30 +0200

    config files

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-snakemake -s 00-scripts/snakefile -j 8 --use-singularity
+snakemake -s 00-scripts/snakefile -j 8 --use-singularity --configfile 01-infos/diplodus_rawdata_config.yaml
 ## clean all files
 rm 02-fastq/* 03-illumina_filter/* 04-phix_filter/* 05-trimmed/*
 rm -Rf 10-logs/*
