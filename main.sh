df8c18039a6b452dbb3c3439f04a546316a5fe70
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-07-16 10:39:01 +0200

    no expand folder

--- main.sh
+++ main.sh
@@ -1,4 +1,9 @@
+## To do a test on tiny data
+snakemake -s 00-scripts/snakefile -j 8 --use-singularity --configfile 01-infos/tiny_config.yaml
+## Run the pipeline on diplodus Sargus fastq raw data
 snakemake -s 00-scripts/snakefile -j 8 --use-singularity --configfile 01-infos/diplodus_rawdata_config.yaml
+## Creating a workflow diagram INSTALLL graphviz
+snakemake --dag | dot -Tsvg > dag.svg
 ## clean all files
 rm 02-fastq/* 03-illumina_filter/* 04-phix_filter/* 05-trimmed/*
 rm -Rf 10-logs/*
