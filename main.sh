2fdc5eb4ebf60e2e8348b853cbaa580ace24f6cf
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-07-19 14:10:16 +0200

    snakemake commands

--- main.sh
+++ main.sh
@@ -5,8 +5,6 @@ snakemake -s 00-scripts/snakefile -j 8 --use-singularity --configfile 01-infos/d
 ## Run the pipeline on mullus Sargus fastq raw data
 snakemake -s 00-scripts/snakefile -j 8 --use-singularity --configfile 01-infos/mullus_rawdata_config.yaml 
 
-
-
 ## Creating a workflow diagram INSTALLL graphviz
 snakemake --dag | dot -Tsvg > dag.svg
 ## clean all files
